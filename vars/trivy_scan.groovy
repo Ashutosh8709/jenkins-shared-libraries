d9da0d9e0bf7d1a8d07b2c4e2577c5b25f6a212d
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-17 23:50:03 +0530

    Update Trivy scan command to use WORKSPACE variable

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,10 +1,9 @@
 def call() {
         sh '''
-        docker exec trivy \
-          trivy fs \
+        docker exec trivy trivy fs \
           --severity HIGH,CRITICAL \
           --exit-code 1 \
           --skip-dirs node_modules,vendor,.git \
-          /workspace/EcommerceCI
+          "$WORKSPACE"
         '''
 }
