667f1c2795cf60010107454121771559c9c917a6
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-17 22:57:52 +0530

    Update Trivy scan command to include server URL

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,8 +1,8 @@
 def call() {
         sh '''
         trivy fs \
+          --server http://host.docker.internal:4954 \
           --severity HIGH,CRITICAL \
           --exit-code 1 \
           .
-        '''
 }
