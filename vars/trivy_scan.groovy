1cea82c2332b51bf55ee99887c3386d2ddd87158
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-17 22:49:16 +0530

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,10 +1,8 @@
 def call() {
-    sh '''
-    docker run --rm \
-      -v "$WORKSPACE:/project" \
-      aquasec/trivy fs \
-      --severity HIGH,CRITICAL \
-      --exit-code 1 \
-      /project
-    '''
+        sh '''
+        trivy fs \
+          --severity HIGH,CRITICAL \
+          --exit-code 1 \
+          .
+        '''
 }
