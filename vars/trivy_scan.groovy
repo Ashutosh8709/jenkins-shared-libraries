8d33353fe272e0fd89287518ea21335a191a916b
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-17 22:43:58 +0530

    Fix volume mount syntax in Trivy scan script

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,7 +1,7 @@
 def call() {
     sh '''
     docker run --rm \
-      -v $PWD:/project \
+      -v "$WORKSPACE:/project" \
       aquasec/trivy fs \
       --severity HIGH,CRITICAL \
       --exit-code 1 \
