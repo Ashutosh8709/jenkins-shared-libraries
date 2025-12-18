f8206be65c92eb6cd27b2e01867c7699d62a4fe0
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-18 11:17:36 +0530

    Enhance SonarQube analysis with scanner options
    Updated the SonarQube analysis script to include environment options for the scanner.

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,14 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
+def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+    withSonarQubeEnv("${SonarQubeAPI}") {
+        withEnv([
+            'SONAR_SCANNER_OPTS=-Xmx2g -Dsonar.scanner.skipSystemTruststore=true'
+        ]) {
+            sh """
+              ${SONAR_HOME}/bin/sonar-scanner \
+                -Dsonar.projectName=${Projectname} \
+                -Dsonar.projectKey=${ProjectKey} \
+                -X
+            """
+        }
+    }
 }
