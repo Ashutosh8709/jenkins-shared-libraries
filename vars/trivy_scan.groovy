6704096ad44e7f7002dd5cb6fdbf93e2a49e4a83
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-18 11:48:27 +0530

    Update Trivy scan path to specific workspace directory

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,9 +1,9 @@
 def call() {
         sh '''
         docker exec trivy trivy fs \
-          --severity HIGH,CRITICAL \
-          --exit-code 1 \
-          --skip-dirs node_modules,vendor,.git \
-          "$WORKSPACE"
+            --severity HIGH,CRITICAL \
+            --exit-code 1 \
+            --skip-dirs node_modules,vendor,.git \
+            /workspace/workspace/EcommerceCI
         '''
 }
