276a10ead1b33cb84c45fcce1249f81a32822106
Ashutosh Kumar <142600069+Ashutosh8709@users.noreply.github.com>
2025-12-18 11:50:26 +0530

    Update Trivy scan to skip additional directory

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,9 +1,9 @@
 def call() {
         sh '''
-        docker exec trivy trivy fs \
+         docker exec trivy trivy fs \
             --severity HIGH,CRITICAL \
             --exit-code 1 \
-            --skip-dirs node_modules,vendor,.git \
+            --skip-dirs node_modules,vendor,.git,EcommerceCI@tmp \
             /workspace/workspace/EcommerceCI
         '''
 }
